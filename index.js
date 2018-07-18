934a11fecc757be1ac74d796e86b06b37e34b129
aasis <aashishrijal84@gmail.com>
2018-07-18 21:42:05 +0545

    setting postgres

--- index.js
+++ index.js
@@ -1,44 +1,12 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rahatweb');
-var Schema = mongoose.Schema;
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser')
+const { Pool } = require('pg')
+require('dotenv').config()
 
-mongoose.Promise = global.Promise;
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to database')
-    });
-
-// Define schema
-var UsersSchema = new Schema({
-    name:  String,
-    address: String,
-    citizenshipid:   String,
-    creationdate: { type: Date, default: Date.now },
-  });
-
-  // Compile model from schema
-var UserModel = mongoose.model('UserModel', UsersSchema)  
-
-// Create an instance of model SomeModel
-var user_instance = new UserModel({
-    name: 'Paurakh Sharma',
-    address: 'Kavrepalanchok',
-    citizenshipid: '1253565',
-    creationdate: Date.now(),
-    })
-
-// Save the new model instance, passing a callback
-user_instance.save(function (err) {
-    if (err) return handleError(err);
-    console.log('Model saved')
-  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -46,18 +14,42 @@ app.use(express.static('public'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-const Users = mongoose.model('users', { name: String });
+
+const pool = new Pool({
+	user: process.env.PGUSER,
+	host: process.env.PGHOST,
+	database: process.env.PGDATABASE,
+	password: process.env.PGPASSWORD,
+	port: process.env.PGPORT,
+	// ssl: true
+});
 
 
+app.listen(4000, () => {
+  console.log("eth is working")
 
-const kitty = new Users({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
+      pool.connect((err, client, done) => {
+        // Handle connection errors
+        if(err) {
+          //done();
+          return console.error(err);
+        }
 
+        pool.query('SELECT * FROM usertb', (err, result) => {
+          if(err){
+              return console.error('error running query', err);
+          }
+          
+          console.log(result.rows)
+        }); 
+      });
+        
 
+});
 
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index',{})
 })
 
 
