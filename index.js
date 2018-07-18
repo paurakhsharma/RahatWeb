8a782092261b668172b66507fcd4b634bde7eff6
aasis <aashishrijal84@gmail.com>
2018-07-18 23:35:55 +0545

    signup code not frontend

--- index.js
+++ index.js
@@ -5,52 +5,52 @@ const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser')
 const { Pool } = require('pg')
 require('dotenv').config()
+const flash = require('connect-flash');
 
+const passport = require("passport")
+const request = require('request');
+const session = require("express-session")
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+var path = require('path');
 
 
-const pool = new Pool({
-	user: process.env.PGUSER,
-	host: process.env.PGHOST,
-	database: process.env.PGDATABASE,
-	password: process.env.PGPASSWORD,
-	port: process.env.PGPORT,
-	// ssl: true
-});
+app.use(flash());
+app.use(session({secret: 'keyboard cat'}))
 
 
-app.listen(4000, () => {
-  console.log("eth is working")
+// app.listen(4000, () => {
+//   console.log("eth is working")
 
-      pool.connect((err, client, done) => {
-        // Handle connection errors
-        if(err) {
-          //done();
-          return console.error(err);
-        }
+//       pool.connect((err, client, done) => {
+//         // Handle connection errors
+//         if(err) {
+//           //done();
+//           return console.error(err);
+//         }
 
-        pool.query('SELECT * FROM usertb', (err, result) => {
-          if(err){
-              return console.error('error running query', err);
-          }
+//         pool.query('SELECT * FROM usertb', (err, result) => {
+//           if(err){
+//               return console.error('error running query', err);
+//           }
           
-          console.log(result.rows)
-        }); 
-      });
+//           console.log(result.rows)
+//         }); 
+//       });
         
 
-});
+// });
 
 
 app.get('/', (req, res) => {
     res.render('index',{})
 })
 
+require('./lib/routes.js')(app)
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
