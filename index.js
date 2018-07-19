a74e441defe92238b96b69025ae4ccf8ff421e6d
PaurakhSharma <paurakh011@gmail.com>
2018-07-19 07:36:11 +0545

    Add routes to map and geojson for Nepal's districts

--- index.js
+++ index.js
@@ -1,64 +1,31 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rahatweb');
-var Schema = mongoose.Schema;
+const { Client } = require('pg')
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser')
 
-mongoose.Promise = global.Promise;
+const client = new Client({
+    connectionString: process.env.DATABASE_URL || 'postgresql://smartmeter:smartmeter@localhost:5432/smartmeter',
+    ssl: true,      
+});
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to database')
-    });
+client.connect((err, client, done) => {
+    if(err) {
+        console.error(err);
+        return
+    }
+});
 
-// Define schema
-var UsersSchema = new Schema({
-    name:  String,
-    address: String,
-    citizenshipid:   String,
-    creationdate: { type: Date, default: Date.now },
-  });
-
-  // Compile model from schema
-var UserModel = mongoose.model('UserModel', UsersSchema)  
-
-// Create an instance of model SomeModel
-var user_instance = new UserModel({
-    name: 'Paurakh Sharma',
-    address: 'Kavrepalanchok',
-    citizenshipid: '1253565',
-    creationdate: Date.now(),
-    })
-
-// Save the new model instance, passing a callback
-user_instance.save(function (err) {
-    if (err) return handleError(err);
-    console.log('Model saved')
-  });
+// Define routes here
+var routes = require('./router')
+app.use('/', routes)
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
-const Users = mongoose.model('users', { name: String });
-
-
-
-const kitty = new Users({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
-
-
-
-
-app.get('/', (req, res) => {
-    res.render('index')
-})
 
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
